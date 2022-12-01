174d91a026e7c83051f166970663544479f787f3
detectivesokka <113736078+detectivesokka@users.noreply.github.com>
2022-11-30 19:57:16 -0500

    Adding ecosystem

--- HealthcareManagement/src/model/EcoSystem.java
+++ HealthcareManagement/src/model/EcoSystem.java
@@ -1,8 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
 
-import java.util.ArrayList;
-import model.HealthcareNetwork.HealthcareNetwork;
+import model.Network.HealthcareNetwork;
 import model.Organization.Organization;
+import model.Role.Role;
+import model.Role.SystemAdminRole;
+import java.util.ArrayList;
 
 /**
  *
@@ -10,7 +17,7 @@ import model.Organization.Organization;
  */
 public class EcoSystem extends Organization {
 
-    private static EcoSystem ecosystem;   
+    private static EcoSystem ecosystem;
     private ArrayList<HealthcareNetwork> networks;
 
     public static EcoSystem getInstance() {
@@ -21,10 +28,46 @@ public class EcoSystem extends Organization {
         return ecosystem;
     }
 
+    public HealthcareNetwork createAndAddNetwork() {
+        
+        HealthcareNetwork network = new HealthcareNetwork();
+        networks.add(network);
+        return network;
+    }
+    
+    public ArrayList<Role> getSupportedRole() {
+        
+        ArrayList<Role> roleList = new ArrayList<Role>();
+        roleList.add(new SystemAdminRole());
+        return roleList;
+    }
+
     private EcoSystem() {
         
         super(null);
         networks = new ArrayList<HealthcareNetwork>();
     }
 
+    public ArrayList<HealthcareNetwork> getNetworks() {
+        
+        return networks;
+    }
+
+    public void setNetworks(ArrayList<HealthcareNetwork> networks) {
+        
+        this.networks = networks;
+    }
+    
+    public HealthcareNetwork searchNetwork(String name) {
+        
+        for(HealthcareNetwork n : this.networks) {
+            
+            if(n.getNetworkName().equals(name)) {
+                
+                return n;
+            }
+        }
+        
+        return null;
+    }   
 }
