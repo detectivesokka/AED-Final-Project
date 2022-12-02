c6628f805e06cc64fe37eca66b38f59f01bc7d79
ganeshkukreja31 <kukreja.g@northeastern.edu>
2022-12-01 22:49:09 -0500

    Added Role for DeliveryMan

---
+++ HealthcareManagement/src/model/Role/DeliveryManRole.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.Role;
+
+import model.DB4OUtil.DB4OUtil;
+import model.EcoSystem;
+import model.Enterprise.Enterprise;
+import model.Organization.DeliveryManOrganization;
+import model.Organization.Organization;
+import model.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.DeliveryManRole.DeliveryManWorkAreaJPanel;
+
+/**
+ *
+ * @author Ganesh Kukreja
+ */
+public class DeliveryManRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount userAccount, Organization organization, Enterprise enterprise, EcoSystem business,DB4OUtil dB4OUtil) {
+        return new DeliveryManWorkAreaJPanel(userProcessContainer, userAccount, (DeliveryManOrganization) organization, enterprise, dB4OUtil, business);
+    }
+
+}
