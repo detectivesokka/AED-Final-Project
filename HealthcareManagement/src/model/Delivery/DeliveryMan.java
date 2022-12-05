85ac04b016187edcb654ca9251bb5a541fb4c76f
ganeshkukreja31 <kukreja.g@northeastern.edu>
2022-12-04 22:06:48 -0500

    Package and class created for DeliveryMan

---
+++ HealthcareManagement/src/model/Delivery/DeliveryMan.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.Delivery;
+
+/**
+ *
+ * @author Ganesh Kukreja
+ */
+public class DeliveryMan {
+    
+    private String deliveryManName;
+    private String contactNo;
+
+    public String getDeliveryManName() {
+        return deliveryManName;
+    }
+
+    public void setDeliveryManName(String deliveryManName) {
+        this.deliveryManName = deliveryManName;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    @Override
+    public String toString() {
+        return deliveryManName;
+    }
+    
+    
+}
